ad3f17c4be50b1803fbc7e3b58e6b3dd383e1acd
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-02 15:59:09 -0800

    made 2 custom directives

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -1,28 +1,72 @@
 angular
-  .module('CardsAgainstAssembly', [])
-  .controller('CardsController', CardsController)
-  .controller('PlayersController', PlayersController);
+    .module('CardsAgainstAssembly', [])
+    .controller('CardsController', CardsController)
+    .controller('PlayersController', PlayersController)
+    .directive('card', CardViewDirective)
+    .directive('scores', ScoresDirective);
 
 function CardsController() {
-  var vm = this;
-  vm.questions = [
-    {question: "What is Batman's guilty pleasure?"},
-    {question: "I'm sorry professor, I couldn't complete my homework because _________."},
-    {question: "I get by with a little help from _________."},
-    {question: "_________. It's a trap!"},
-    {question: "The class field trip was completely ruined by _________."},
-    {question: "What's my secret power?"}
-  ];
+    var vm = this;
+    vm.questions = [{
+        question: "What is Batman's guilty pleasure?"
+    }, {
+        question: "I'm sorry professor, I couldn't complete my homework because _________."
+    }, {
+        question: "I get by with a little help from _________."
+    }, {
+        question: "_________. It's a trap!"
+    }, {
+        question: "The class field trip was completely ruined by _________."
+    }, {
+        question: "What's my secret power?"
+    }];
 }
 
-function PlayersController(){
-  var vm = this;
-  vm.players = [
-    {name: 'Gilligan', points: 2},
-    {name: 'Mary Ann', points: 16},
-    {name: 'Ginger', points: 12},
-    {name: 'Thurson', points: 6},
-    {name: 'Skipper', points: 9},
-    {name: 'The Professor', points: 21}
-  ];
+function PlayersController() {
+    var vm = this;
+    vm.players = [{
+        name: 'Gilligan',
+        points: 2
+    }, {
+        name: 'Mary Ann',
+        points: 16
+    }, {
+        name: 'Ginger',
+        points: 12
+    }, {
+        name: 'Thurson',
+        points: 6
+    }, {
+        name: 'Skipper',
+        points: 9
+    }, {
+        name: 'The Professor',
+        points: 21
+    }];
+}
+
+function CardViewDirective() {
+    var directive = {
+        restrict: 'E',
+        replace: true,
+        templateUrl: "./_cardView.html",
+        scope: {
+            question: '@'
+        }
+    };
+
+    return directive;
+}
+
+function ScoresDirective(){
+  var directive = {
+    restrict: 'E',
+    replace: true,
+    templateUrl: './_scoresView.html',
+    scope: {
+      name: '@',
+      points: '@'
+    }
+  }
+  return directive;
 }
